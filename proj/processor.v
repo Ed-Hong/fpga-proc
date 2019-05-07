4541db068345bf7f9d687476cf7a7f3013d80d5e
Edward Hong <edward.xc.hong@gmail.com>
2019-05-07 16:33:00 -0500

    add debug lines for debugging decode

--- proj/processor.v
+++ proj/processor.v
@@ -56,16 +56,25 @@ module processor(
 				4'b0001:begin
 							write_data <= immediate;
 							write_enable <= 1;
+							
+							//debug
+							result <= 8'hFFFFFF01;
 						end
 				// add
 				4'b0010:begin
 							//todo - move this to ALU
 							write_data <= (data_a + data_b); 
 							write_enable <= 1;
+							
+							//debug
+							result <= 8'hFFFFFF03;
 						end
 				// out
 				4'b1111:begin
 							//result <= data_a;
+							
+							//debug
+							result <= 8'hFFFFFF0F;
 						end
 				// default NOP
 				default: result <= 8'hFFFFFFAA;  
@@ -74,7 +83,7 @@ module processor(
 			write_enable <= 0;
 			
 			//debug
-			result <= instruction;
+			//result <= instruction;
 			
 		end
 	end
