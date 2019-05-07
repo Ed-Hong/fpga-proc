393196865983fbc046202e29f62bb625638ff537
Edward Hong <edward.xc.hong@gmail.com>
2019-05-07 18:54:21 -0500

    fine tune 7seg freq

--- proj/machine.v
+++ proj/machine.v
@@ -32,29 +32,29 @@ module machine(
 	// assign I/O
 	always @ (posedge clk) begin
 		timer = timer + 1;
-		if (timer >= 5000000) begin
+		if (timer >= 500000) begin
 			timer = 0;
 		end
 		
 		//todo 7seg encoding
 		
 		// manual delay to handle 7seg anode output
-		if(timer == 0500000) begin
+		if(timer == 100000) begin
 			//out <= result[3:0];
 			out <= 8'b11000000;
 			an <= 4'b1110;
 		end
-		if(timer == 1000000) begin
+		if(timer == 200000) begin
 			//out <= result[7:4];
 			out <= 8'b11111001;
 			an <= 4'b1101;
 		end
-		if(timer == 1500000) begin
+		if(timer == 300000) begin
 			//out <= result[11:8];
 			out <= 8'b10100100;
 			an <= 4'b1011;
 		end
-		if(timer == 2000000) begin
+		if(timer == 400000) begin
 			//out <= result[15:12];
 			out <= 8'b10110000;
 			an <= 4'b0111;
