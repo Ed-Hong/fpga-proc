07b374d0d5f8227f508504bcd8bce0732951f854
Edward Hong <edward.xc.hong@gmail.com>
2019-05-08 01:53:56 -0500

    this time actually test jmp

--- proj/program_rom.v
+++ proj/program_rom.v
@@ -12,7 +12,7 @@ module program_rom(
 		inst_mem[0] 	= 16'b0001_0010_0000_0010;	//load r1 2
 		inst_mem[1] 	= 16'b0010_0010_0100_0000;	//add r1 r1
 		inst_mem[2] 	= 16'b1111_0010_0000_0000;	//out r1
-		inst_mem[3] 	= 16'b1100_0000_0000_0000;	//jmp addr0
+		inst_mem[3] 	= 16'b1000_0000_0000_0000;	//jmp addr0
 		
 		inst_mem[4] 	= 16'b0010_0100_1000_1111;	//add r2 F
 		inst_mem[5] 	= 16'b1111_0100_0000_0000;	//out r2
