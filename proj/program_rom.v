17c1b10696ee8be3f73ba75cef12c6ac969973e3
Edward Hong <edward.xc.hong@gmail.com>
2019-05-07 20:37:48 -0500

    update program rom

--- proj/program_rom.v
+++ proj/program_rom.v
@@ -9,14 +9,14 @@ module program_rom(
 	reg [15:0] inst_mem [7:0];
 	
 	initial begin
-		inst_mem[0] = 16'b0001_0010_0011_0011;	//addi r1 
-		inst_mem[1] = 16'b0001_0100_1100_1100;	//addi r2 
+		inst_mem[0] = 16'b0001_0010_0010_0000;	//addi r1 32
+		inst_mem[1] = 16'b0001_0100_0000_1010;	//addi r2 10
 		inst_mem[2] = 16'b0010_0010_1000_0001;	//add r1 r2
-		inst_mem[3] = 16'b1111_0010_0000_0001;	//out r1
-		inst_mem[4] = 16'b1111_0010_0000_0001;	//out r1
-		inst_mem[5] = 16'b1111_0100_0000_0010;	//out r2
-		inst_mem[6] = 16'b1111_0100_0000_0010;	//out r2
-		inst_mem[7] = 16'b1111_0100_0000_0010;	//out r2
+		inst_mem[3] = 16'b1111_0010_0000_0000;	//out r1
+		inst_mem[4] = 16'b1111_0010_0000_0000;	//out r1
+		inst_mem[5] = 16'b1111_0010_0000_0000;	//out r1
+		inst_mem[6] = 16'b1111_0010_0000_0000;	//out r1
+		inst_mem[7] = 16'b1111_0010_0000_0000;	//out r1
 	end 
 	
 	assign instruction = inst_mem[address];
