158a5a94b91af5231ea5c1f8273a4cef64a01ba7
Edward Hong <edward.xc.hong@gmail.com>
2019-05-07 21:11:59 -0500

    debug sub

--- proj/program_rom.v
+++ proj/program_rom.v
@@ -10,7 +10,7 @@ module program_rom(
 	
 	initial begin
 		inst_mem[0] = 16'b0001_0010_0000_1010;	//addi r1 10
-		inst_mem[1] = 16'b0001_0100_0000_1010;	//addi r2 10
+		inst_mem[1] = 16'b0001_0100_0010_0000;	//addi r2 32
 		inst_mem[2] = 16'b0011_0010_1000_0000;	//sub r1 r2
 		inst_mem[3] = 16'b1111_0010_0000_0000;	//out r1
 		inst_mem[4] = 16'b1111_0010_0000_0000;	//out r1
