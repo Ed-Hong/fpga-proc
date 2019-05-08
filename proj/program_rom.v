1e506e7afb2127665c479de1839a53d7ee1c0499
Edward Hong <edward.xc.hong@gmail.com>
2019-05-08 01:46:18 -0500

    test branch

--- proj/program_rom.v
+++ proj/program_rom.v
@@ -10,14 +10,12 @@ module program_rom(
 	
 	initial begin
 		inst_mem[0] 	= 16'b0001_0010_0000_0010;	//addi r1 2
-		inst_mem[1] 	= 16'b0010_0010_0100_0000;	//add r1 r1
+		inst_mem[1] 	= 16'b0011_0010_0100_0000;	//sub r1 r1
 		inst_mem[2] 	= 16'b1111_0010_0000_0000;	//out r1
-
+		inst_mem[3] 	= 16'b1100_0000_0000_0000;	//br addr0
 		
-		inst_mem[3] 	= 16'b0001_0100_0000_0101;	//addi r2 5
-		inst_mem[4] 	= 16'b0010_0100_1000_0000;	//add r2 r2
+		inst_mem[4] 	= 16'b0010_0100_1000_1111;	//add r2 F
 		inst_mem[5] 	= 16'b1111_0100_0000_0000;	//out r2
-
 		
 		inst_mem[6] 	= 16'b0001_0110_0000_1000;	//addi r3 8
 		inst_mem[7] 	= 16'b0010_0110_1100_0000;	//add r3 r3
